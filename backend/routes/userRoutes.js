ec588fc019d9b88806c7b628e356b3e26eb9baf1
Gm Dev <gmmoose17@gmail.com>
2024-07-07 03:04:54 +0800

    feat: Implement user routes for registration and login endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,16 @@
-// const express = require("express");
-// const {
-// 	registerUser,
-// 	authUser,
-// 	allUsers,
-// } = require("../controllers/userControllers");
-// const { protect } = require("../middleware/authMiddleware");
+const express = require("express");
+const {
+	registerUser,
+	authUser,
+	allUsers,
+} = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
 
-// const router = express.Router();
+const router = express.Router();
 
-// // Registration endpoint
-// router.route("/").post(registerUser).get(protect, allUsers);
-// // Login endpoint
-// router.post("/login", authUser);
+// Registration endpoint
+router.route("/").post(registerUser).get(protect, allUsers);
+// Login endpoint
+router.post("/login", authUser);
 
-// module.exports = router;
+module.exports = router;
