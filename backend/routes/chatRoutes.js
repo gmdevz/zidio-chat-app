7130f27764db70d168e35353d41921a55f6d3c71
Gm Dev <gmmoose17@gmail.com>
2024-07-07 02:50:25 +0800

    commented out for testing

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const { protect } = require("../middleware/authMiddleware");
-const {
-	accessChat,
-	fetchChats,
-	createGroupChats,
-	renameGroup,
-	addToGroup,
-	removeFromGroup,
-} = require("../controllers/chatControllers");
+// const express = require("express");
+// const { protect } = require("../middleware/authMiddleware");
+// const {
+// 	accessChat,
+// 	fetchChats,
+// 	createGroupChats,
+// 	renameGroup,
+// 	addToGroup,
+// 	removeFromGroup,
+// } = require("../controllers/chatControllers");
 
-const router = express.Router();
+// const router = express.Router();
 
-router.route("/").post(protect, accessChat);
-router.route("/").get(protect, fetchChats);
-// creating a group
-router.route("/group").post(protect, createGroupChats);
-// rename group
-router.route("/rename").put(protect, renameGroup);
-// add someone to group
-router.route("/groupadd").put(protect, addToGroup);
-// remove someone from group
-router.route("/groupremove").put(protect, removeFromGroup);
+// router.route("/").post(protect, accessChat);
+// router.route("/").get(protect, fetchChats);
+// // creating a group
+// router.route("/group").post(protect, createGroupChats);
+// // rename group
+// router.route("/rename").put(protect, renameGroup);
+// // add someone to group
+// router.route("/groupadd").put(protect, addToGroup);
+// // remove someone from group
+// router.route("/groupremove").put(protect, removeFromGroup);
 
-module.exports = router;
+// module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const { chats } = require("./data/data");
-const connectDB = require("./config/db");
-const colors = require("colors");
-const userRoutes = require("./routes/userRoutes");
-const chatRoutes = require("./routes/chatRoutes");
-const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+// const express = require("express");
+// const dotenv = require("dotenv");
+// const { chats } = require("./data/data");
+// const connectDB = require("./config/db");
+// const colors = require("colors");
+// const userRoutes = require("./routes/userRoutes");
+// const chatRoutes = require("./routes/chatRoutes");
+// const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
-dotenv.config();
-connectDB();
-const app = express();
+// dotenv.config();
+// connectDB();
+// const app = express();
 
-// to accept json data
-app.use(express.json());
+// // to accept json data
+// app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.send("API is Running");
-});
+// app.get("/", (req, res) => {
+// 	res.send("API is Running");
+// });
 
-app.use("/api/user", userRoutes);
-app.use("/api/chat", chatRoutes);
+// app.use("/api/user", userRoutes);
+// app.use("/api/chat", chatRoutes);
 
-app.use(notFound);
-app.use(errorHandler);
+// app.use(notFound);
+// app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
+// app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
