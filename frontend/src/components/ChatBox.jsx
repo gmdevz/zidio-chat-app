b9632a28f625970ea819dd1fc670f4150c579856
Gm Dev <gmmoose17@gmail.com>
2024-07-09 02:27:05 +0800

    Refactor ChatBox component to use Chakra UI Box for styling and implement dynamic display based on selectedChat state

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -1,8 +1,24 @@
-const ChatBox = () => {
+import { Box } from "@chakra-ui/react";
+import { ChatState } from "../Context/ChatProvider";
+import SingleChat from "./SingleChat";
+
+const ChatBox = ({ fetchAgain, setFetchAgain }) => {
+	const { selectedChat } = ChatState();
 	return (
-		<div>
-			<h1>ChatBox</h1>
-		</div>
+		<>
+			<Box
+				display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+				alignItems="center"
+				flexDir="column"
+				p={3}
+				bg="white"
+				width={{ base: "100%", md: "68%" }}
+				borderRadius="lg"
+				borderWidth="1px"
+			>
+				<SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+			</Box>
+		</>
 	);
 };
 
