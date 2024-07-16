8770597e11db89345dddad794b28da75a4d91cb7
Gm Dev <gmmoose17@gmail.com>
2024-07-16 12:56:40 +0800

    Added notification badge and improve notification handling in SideDrawer

--- frontend/src/components/miscellaneous/SideDrawer.jsx
+++ frontend/src/components/miscellaneous/SideDrawer.jsx
@@ -21,12 +21,15 @@ import {
 	useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ChatState } from "../../Context/ChatProvider";
 import ChatLoading from "../ChatLoading";
 import UserListItem from "../UserAvatar/UserListItem";
 import ProfileModal from "./ProfileModal";
+import { getSender } from "../../config/ChatLogic";
+import NotificationBadge from "react-notification-badge";
+import { Effect } from "react-notification-badge";
 
 const SideDrawer = () => {
 	const [search, setSearch] = useState("");
@@ -34,7 +37,14 @@ const SideDrawer = () => {
 	const [loading, setLoading] = useState(false);
 	const [loadingChat, setLoadingChat] = useState();
 
-	const { user, setSelectedChat, chats, setChats } = ChatState();
+	const {
+		user,
+		setSelectedChat,
+		chats,
+		setChats,
+		notification,
+		setNotification,
+	} = ChatState();
 
 	const navigate = useNavigate();
 
@@ -44,10 +54,35 @@ const SideDrawer = () => {
 		localStorage.removeItem("userInfo");
 		navigate("/");
 	};
+	// const fetchNotifications = async () => {
+	// 	if (!user) return;
 
+	// 	try {
+	// 		const config = {
+	// 			headers: {
+	// 				"Content-Type": "application/json",
+	// 				Authorization: `Bearer ${user.token}`,
+	// 			},
+	// 		};
+
+	// 		const { data } = await axios.get("/api/notification", config);
+	// 		// console.log("Fetched notifications:", data);
+	// 		setNotification(data);
+	// 	} catch (error) {
+	// 		toast({
+	// 			title: "Error Occured!",
+	// 			description: "Failed to load notifications",
+	// 			status: "error",
+	// 			duration: 5000,
+	// 			isClosable: true,
+	// 			position: "bottom-left",
+	// 		});
+	// 	}
+	// };
 	const toast = useToast();
 
 	const handleClose = () => {
+		setSearch("");
 		setSearchResult([]);
 		onClose();
 	};
@@ -133,6 +168,33 @@ const SideDrawer = () => {
 		}
 	};
 
+	const handleNotificationClick = async (notif) => {
+		try {
+			await axios.put(
+				`/api/notification/${notif._id}`,
+				{},
+				{
+					headers: {
+						Authorization: `Bearer ${user.token}`,
+					},
+				},
+			);
+			setSelectedChat(notif.chat);
+			setNotification((prevNotifications) =>
+				prevNotifications.filter((n) => n._id !== notif._id),
+			);
+		} catch (error) {
+			toast({
+				title: "Error Occured!",
+				description: "Failed to mark notification as read",
+				status: "error",
+				duration: 5000,
+				isClosable: true,
+				position: "bottom",
+			});
+		}
+	};
+
 	return (
 		<>
 			<Box
@@ -159,9 +221,42 @@ const SideDrawer = () => {
 				<div>
 					<Menu>
 						<MenuButton p={1}>
+							<NotificationBadge
+								count={notification.length}
+								effect={Effect.SCALE}
+							/>
 							<BellIcon fontSize="2xl" m={1} />
+							{/* {notification.length > 0 && (
+								<span
+									style={{
+										backgroundColor: "red",
+										borderRadius: "50%",
+										padding: "2px 6px",
+										color: "white",
+										fontSize: "12px",
+									}}
+								>
+									{notification.length}
+								</span>
+							)} */}
 						</MenuButton>
-						{/* <MenuList></MenuList> */}
+						<MenuList pl={2}>
+							{!notification.length && "No New Messages"}
+							{notification.map((notif) => (
+								<MenuItem
+									key={notif._id}
+									onClick={() => {
+										setSelectedChat(notif.chat);
+										setNotification(notification.filter((n) => n !== notif));
+										handleNotificationClick(notif);
+									}}
+								>
+									{notif.chat.isGroupChat
+										? `New Message in ${notif.chat.chatName}`
+										: `New Message from ${notif.sender.name}`}
+								</MenuItem>
+							))}
+						</MenuList>
 					</Menu>
 					<Menu>
 						<MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
