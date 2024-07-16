0f13a8b713177cbff1c6473e2ea4ac6eb87606a0
Gm Dev <gmmoose17@gmail.com>
2024-07-16 16:45:23 +0800

    Modify for deployment

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
 	pingTimeout: 60000,
 	cors: {
-		origin: "http://localhost:5173", ""
+		origin: "http://localhost:5173", // for development
 	},
 });
 
