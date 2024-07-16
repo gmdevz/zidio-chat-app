a24fac0b849d9dc5be1862cb858621b79a646b48
Gm Dev <gmmoose17@gmail.com>
2024-07-16 12:53:00 +0800

    Created Notification model schema for user alertsd notification

---
+++ backend/models/notificationModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = mongoose.Schema(
+	{
+		sender: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+		recipient: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+		content: {
+			type: String,
+			trim: true,
+		},
+		chat: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Chat",
+		},
+		read: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+module.exports = Notification;
