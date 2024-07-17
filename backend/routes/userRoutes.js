7a9d1a4a91cfe687b7c75836884da87cd7079d17
Gm Dev <gmmoose17@gmail.com>
2024-07-17 22:58:57 +0800

    Added a new routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,10 @@ const {
 	registerUser,
 	authUser,
 	allUsers,
+	guestLogin,
+	renameUser,
+	getOnlineUsers,
+	deleteGuestUser,
 } = require("../controllers/userControllers");
 const { protect } = require("../middleware/authMiddleware");
 
@@ -12,5 +16,13 @@ const router = express.Router();
 router.route("/").post(registerUser).get(protect, allUsers);
 // Login endpoint
 router.post("/login", authUser);
+
+router.get("/online", protect, getOnlineUsers);
+
+router.post("/guestlogin", guestLogin);
+
+router.put("/rename", protect, renameUser);
+
+router.delete("/guestlogout", protect, deleteGuestUser);
 
 module.exports = router;
