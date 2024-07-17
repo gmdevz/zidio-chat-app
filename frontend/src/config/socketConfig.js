3e3dedb76935c888ef7b99fe941a087d6a73f476
Gm Dev <gmmoose17@gmail.com>
2024-07-17 23:48:49 +0800

    Modify for deployment

--- frontend/src/config/socketConfig.js
+++ frontend/src/config/socketConfig.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
 
-const ENDPOINT = "http://192.168.100.2:5000";
+const ENDPOINT = "http://localhost:5000";
 export const socket = io(ENDPOINT);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
 				rewrite: (path) => path.replace(/^\/api/, "/api"),
 			},
 		},
-		host: true,
+		// host: true,
 	},
 	plugins: [react()],
 });
