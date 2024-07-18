3d04053873a73da5036aa5169c49fe00d7bb64b1
Gm Dev <gmmoose17@gmail.com>
2024-07-18 12:06:03 +0800

    modified for deployment

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
 	pingTimeout: 60000,
 	cors: {
-		origin: "http://localhost:5173",
+		origin: "https://zidio-chat-app.onrender.com/", // origin is the URL of your frontend app
 	},
 });
 
